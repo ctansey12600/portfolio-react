32819e93ce3923ac261b3c5b7089a4e4f1af4c36
Casey Tansey <ctansey12600@outlook.com>
2022-08-17 21:03:59 -0400

    Update

--- src/components/pages/QuestionForm.js
+++ src/components/pages/QuestionForm.js
@@ -242,17 +242,6 @@ function QuestionForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    }).get("/db", async (req, res) => {
-      try {
-        const client = await pool.connect();
-        const result = await client.query("SELECT * FROM test_table");
-        const results = { results: result ? result.rows : null };
-        res.render("pages/db", results);
-        client.release();
-      } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-      }
     });
     setFormData({
       firstName: "",
